aed4e74319007ba907cff9ad80182ca45f9643f3
naddiluv <nadiaguerrc@gmail.com>
2025-09-29 10:08:14 -0600

    avance

--- main.js
+++ main.js
@@ -12,19 +12,19 @@ const songs = [
             song_name : "cancion CHIDA",
             artist_name: "artista 1",
             song_url: "./media/song.mp3",
-            caratula: "https://picsum.photos/200"
+            caratula: "/music_app_practice/media/song.jpg"
         },
         {
             song_name : "cancion CHIDA 2",
             artist_name: "artista 1",
             song_url: "./media/song_2.mp3",
-            caratula: "https://picsum.photos/200"
+            caratula: "/music_app_practice/media/song2.jpg"
         },
         {
             song_name : "cancion CHIDA 3",
             artist_name: "artista 3",
             song_url: "./media/song_3.mp3",
-            caratula: "https://picsum.photos/200"
+            caratula: "/music_app_practice/media/song3.jpg"
         },
     ];
 
